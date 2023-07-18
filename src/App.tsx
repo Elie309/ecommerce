60798950481bc09593c851ebdc775d702de99f12
Elie Saade <elie.saade432@gmail.com>
2023-07-18 11:16:40 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -17,8 +17,8 @@ function App() {
 {/*       <div className="m-10 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
         {products.map((product) => (
           <ProductElement key={product.id} {...product} />
-        ))} */}
-      </div>
+        ))} 
+      </div>*/}
 
     </MainLayout>
   )
