ec0971a2abbd3e3bc15bf0e6c5e27d091f0c85a9
elie309 <elie.saade432@gmail.com>
2023-07-30 13:06:52 +0300

    Header, main and footer set

--- src/components/Layout/MainLayout.tsx
+++ src/components/Layout/MainLayout.tsx
@@ -9,14 +9,13 @@ interface Props {
 
 export default function MainLayout({ children, className }: Props) {
   return (
-    <main className={"h-full w-full flex flex-col justify-between" + " " + className}>
-      <div>
-        <Navigation />
+    <>
+      <Navigation />
+      <main className={`min-h-full w-full ${className}`}>
         {children}
-      </div>
-      <div>
-        <Footer />
-      </div>
-    </main>
+      </main >
+      <Footer />
+    </>
+
   )
 }
--- src/components/Layout/Navigation.tsx
+++ src/components/Layout/Navigation.tsx
@@ -20,49 +20,53 @@ export default function Navigation() {
 
 
   return (
-    <nav className="flex justify-between bg-gray-900 px-4 py-2 md:py-4 md:px-8">
-      <Link to="/" className="text-white text-xl md:text-2xl font-bold">
-        Techology
-      </Link>
+    <header>
 
-      <div className="flex justify-around w-2/6 pr-3">
-
-        <div className="relative hidden md:block">
-          <SearchIcon
-            className="absolute fill-white"
-            height={ICON_HEIGHT_SEARCH}
-            width={ICON_WIDTH_SEARCH}
-
-          />
-          <input
-            type="text"
-            placeholder="Search"
-            className="pl-8 pb-2 bg-transparent border-b-2 border-white text-white placeholder-white::placeholder focus:outline-none"
-          />
-        </div>
-
-        <Link to="/cart" className="text-white">
-          <CartIcon
-            className="absolute ml-1 fill-white"
-            height={26}
-            width={26}
-          />
+      <nav className="flex justify-between bg-gray-900 px-4 py-2 md:py-4 md:px-8">
+        <Link to="/" className="text-white text-xl md:text-2xl font-bold">
+          Techology
         </Link>
 
-        {
-          isAuthenticated ?
-            (
-              <ProfileNavigationItems handleDropDownClick={handleDropDownClick} />
-            )
-            :
-            (
-              <Link to="/login" className="text-white">
-                Login
-              </Link>
-            )
-        }
-      </div>
-    </nav >
+        <div className="flex justify-around w-2/6 pr-3">
+
+          <div className="relative hidden md:block">
+            <SearchIcon
+              className="absolute fill-white"
+              height={ICON_HEIGHT_SEARCH}
+              width={ICON_WIDTH_SEARCH}
+
+            />
+            <input
+              type="text"
+              placeholder="Search"
+              className="pl-8 pb-2 bg-transparent border-b-2 border-white text-white placeholder-white::placeholder focus:outline-none"
+            />
+          </div>
+
+          <Link to="/cart" className="text-white">
+            <CartIcon
+              className="absolute ml-1 fill-white"
+              height={26}
+              width={26}
+            />
+          </Link>
+
+          {
+            isAuthenticated ?
+              (
+                <ProfileNavigationItems handleDropDownClick={handleDropDownClick} />
+              )
+              :
+              (
+                <Link to="/login" className="text-white">
+                  Login
+                </Link>
+              )
+          }
+        </div>
+      </nav >
+    </header>
+
   );
 }
 
